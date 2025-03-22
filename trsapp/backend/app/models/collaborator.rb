7de3bb1f8f367512f0775f47d78d2787c734e1b2
8t104k <8t104k@gmail.com>
2025-03-23 00:19:03 +0900

    コラボレーターにEnum情報を追加

--- trsapp/backend/app/models/collaborator.rb
+++ trsapp/backend/app/models/collaborator.rb
@@ -1,4 +1,17 @@
 class Collaborator < ApplicationRecord
   belongs_to :event
   belongs_to :profile
+
+  enum role: {
+    organizer: "org",
+    member: "mem",
+    staff: "stf",
+    livehouse: "lvh"
+  }
+  enum access_status: {
+    active: "act",
+    inactive: "ina",
+    pending: "pnd",
+    inviting: "inv"
+  }
 end
