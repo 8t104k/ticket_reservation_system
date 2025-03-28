29adb601df1e332146cf265e8f77eb9fb30e9584
8t104k <8t104k@gmail.com>
2025-03-28 19:56:58 +0900

    Indexメソッドのレスポンスを修正

--- trsapp/backend/app/controllers/api/v1/collaborators_controller.rb
+++ trsapp/backend/app/controllers/api/v1/collaborators_controller.rb
@@ -3,7 +3,12 @@ class Api::V1::CollaboratorsController < ApplicationController
     @event = Event.find_by!(token: params[:event_token])
     # @profiles = @event.profiles.all
     @collaborators = @event.collaborators.includes(:profile, :group)
-    render json: @collaborators, include: %i[profile group]
+    @current_collaborator = @collaborators.find_by(profile_id: @current_user.id)
+    response_data = {
+      collaborators: @collaborators.as_json(include: [:profile, :group]),
+      current_collaborator: @current_collaborator.as_json
+    }
+    render json: response_data
   end
   
 end
